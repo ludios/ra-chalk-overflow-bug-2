241bd7d9bca3216b0ed62c86f9fa7bc47201d825
Ivan Kozik <ivan@ludios.org>
2022-01-29 08:26:48 +0000

    Revert "Variant that doesn't bother rust-analyzer"
    This reverts commit 6e81368870f1bc21041207e615c5cb3780b6b5dd.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use aes::Aes128Ctr;
 use cipher::NewCipher;
 
-pub fn fail_to_make_ra_unhappy() {
+type Aes128Ctr = ctr::Ctr64BE<aes::Aes128>;
+
+pub fn make_ra_unhappy() {
     let zeroes = [0; 16].into();
     let _cipher = Aes128Ctr::new(&zeroes, &zeroes);
 }
