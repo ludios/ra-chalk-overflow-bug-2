719695458af13662d2359109281b8100ace1db82
Ivan Kozik <ivan@ludios.org>
2022-01-29 08:07:22 +0000

    Reduce

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,8 @@
-#![feature(
-    generators,
-    proc_macro_hygiene,
-    stmt_expr_attributes,
-)]
-
 use cipher::NewCipher;
-use aes::cipher::generic_array::GenericArray;
-use futures::stream::BoxStream;
-use futures_async_stream::stream;
 
 type Aes128Ctr = ctr::Ctr64BE<aes::Aes128>;
 
-pub fn stream_chunks() -> BoxStream<'static, ()> {
-    Box::pin(
-        #[stream]
-        async move {
-            let zeroes = GenericArray::from_slice(&[0; 16]);
-            let _cipher = Aes128Ctr::new(zeroes, zeroes);
-        }
-    )
+pub fn make_ra_unhappy() {
+    let zeroes = [0; 16].into();
+    let _cipher = Aes128Ctr::new(&zeroes, &zeroes);
 }
