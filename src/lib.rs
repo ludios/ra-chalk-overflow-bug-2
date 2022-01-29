6e81368870f1bc21041207e615c5cb3780b6b5dd
Ivan Kozik <ivan@ludios.org>
2022-01-29 08:26:26 +0000

    Variant that doesn't bother rust-analyzer

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
+use aes::Aes128Ctr;
 use cipher::NewCipher;
 
-type Aes128Ctr = ctr::Ctr64BE<aes::Aes128>;
-
-pub fn make_ra_unhappy() {
+pub fn fail_to_make_ra_unhappy() {
     let zeroes = [0; 16].into();
     let _cipher = Aes128Ctr::new(&zeroes, &zeroes);
 }
